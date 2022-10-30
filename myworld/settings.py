0522fbc6784e35205e26b8ed804faa0f996b250a
Yazel Hipolito <yazelhipolito@gmail.com>
2022-10-30 09:09:56 +0300

    remove Django security key

--- myworld/settings.py
+++ myworld/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_2zzhf52f4+8+3k^4hpz1quz)!*c$us5rwpo&bw_@i5-btafg('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
